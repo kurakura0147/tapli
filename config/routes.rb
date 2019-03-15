ad19206f5a9994df4c58fad621dbf4eeb00d4b1e
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-03-15 17:09:17 +0900

    About , sampleページの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'datas#index'
-  resources :datas, only: %i(index)
+  root 'datas#about'
+  resources :datas, only: %i(index show) do
+    collection do
+      get :about
+      get :sample
+    end
+  end
+
 end
