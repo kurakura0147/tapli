7187ba49ca8827bb1b5fea6cd324de1e63ae3e2c
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-03-20 01:07:21 +0900

    Dataテーブルにカラムの追加と削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_194108) do
+ActiveRecord::Schema.define(version: 2019_03_19_150711) do
 
   create_table "data", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "weight"
     t.string "height"
-    t.string "bmi"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "record_day"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_data_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
