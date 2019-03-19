675c5ae6959ef90b91c0326d03b2827d522bfa5c
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-03-20 01:07:54 +0900

    データ入力機能の実装

--- app/controllers/datas_controller.rb
+++ app/controllers/datas_controller.rb
@@ -1,20 +1,41 @@
 class DatasController < ApplicationController
 
+    before_action :authenticate_user!, except: [:sample, :about ]
+
+
   def index
   end
 
+  def new
+    @data = Datum.new
+  end
+
+  def create
+    @data = Datum.new(params_datum)
+    @data.save!
+    redirect_to root_path
+  end
+
   def show
+    @data = Datum.find(1)
   end
 
   def about
   end
 
   def sample
-    @add = [9]
+    @add = [10]
     gon.add = @add
-    @data = [1,2,3]
+    @data = [1,9,3]
     gon.data = @data
     gon.data << gon.add
   end
+
+  private
+
+  def params_datum
+    params.require(:datum).permit(:weight, :height, :record_day).merge(user_id: current_user.id)
+  end
+
 
 end
