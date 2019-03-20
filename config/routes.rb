ff13a7dd42110d8147b3e80513ce3eca1d827ad8
kurakura0147 <tabatayuuta@usernoMacBook-Pro.local>
2019-03-20 16:21:08 +0900

    Routesの表記の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
 
+  devise_for :users
 
   root 'datas#about'
-  resources :datas, only: [:index, :new, :create] do
+  resources :datas, only: %i(index new create) do
     collection do
       get :about
       get :sample
     end
   end
 
-  devise_for :users
   resources :users, only: %i(show)
 
 end
